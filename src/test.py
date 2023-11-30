27c9f3587afef853e37eec63460d2bbaa2647d85
Emilia <contact@emiliaallison.com>
2023-11-29 20:22:34 -0500

    Comments for a200_string test

--- src/test.py
+++ src/test.py
@@ -5,10 +5,10 @@ import a200_string
 class TestA200String(unittest.TestCase):
     def test_transferlist_to_a200_strings(self):
         tl = [
-            ('A', 0, 0, 0, 0),
-            ('A', 0, 0, 1, 0),
-            ('A', 0, 0, 0, 3),
-            ('A', 0, 0, 4, 3),
+            ('A', 0, 0, 0, 0),  # 1st bit is 1
+            ('A', 0, 0, 1, 0),  # No effect
+            ('A', 0, 0, 0, 3),  # 4th bit is 1
+            ('A', 0, 0, 4, 3),  # (12+4th) bit is 1
         ]
 
         res = a200_string.transferlist_to_a200_strings(tl)
